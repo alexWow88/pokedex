24c890273133863ede8f1de380147fd639491643
Alex Wower <woweralexander@gmail.com>
2022-01-22 22:37:08 +0000

    pokemon backs rendering

--- src/components/PokedexCard/PokedexCard.jsx
+++ src/components/PokedexCard/PokedexCard.jsx
@@ -3,33 +3,33 @@ import "./PokedexCard.scss";
 import { useState, useEffect } from "react";
 
 const PokedexCard = ({ pokemon }) => {
-  // const [pokemonInfo, setPokemonInfo] = useState([]);
+  const [pokemonInfo, setPokemonInfo] = useState([]);
 
-  // const fetchPokemonInfo = () => {
-  //   fetch(`https://pokeapi.co/api/v2/${pokemon.name}`)
-  //     .then((response) => response.json())
-  //     .then((jsonResponse) => {
-  //       console.log(jsonResponse);
-  //       setPokemonInfo(jsonResponse);
-  //     })
-  //     .catch((err) => console.log("err"));
-  // };
+  const fetchPokemonInfo = () => {
+    fetch(`${pokemon.url}`)
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        console.log(jsonResponse);
+        setPokemonInfo(jsonResponse);
+      })
+      .catch((err) => console.log("err"));
+  };
 
-  // useEffect(() => {
-  //   fetchPokemonInfo();
-  // }, []);
+  useEffect(() => {
+    fetchPokemonInfo();
+  }, []);
 
   return (
     <>
       <div className="pokedexCard">{pokemon.name}</div>
-      {/* <img
+      <img
         className="pokedexCard__img"
         src={
           pokemonInfo
             ? pokemonInfo.sprites.back_default
             : "https://upload.wikimedia.org/wikipedia/commons/4/4c/Pokeball.png"
         }
-      /> */}
+      />
     </>
   );
 };
